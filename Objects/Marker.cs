475ddfe9f4bdac3f34d8c429674a88ec2382a0ff
PurrOfCat <sonya29.04.02@gmail.com>
2021-12-04 20:09:30 +0800

    Добавьте файлы проекта.

---
+++ Objects/Marker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Text;
+
+namespace Lab5.Objects
+{
+    class Marker : BaseObject
+    {
+        public Marker(float x, float y, float angle) : base(x, y, angle) {}
+
+        public override void Render(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Red), -3, -3, 6, 6);
+            g.DrawEllipse(new Pen(Color.Red, 2), -6, -6, 12, 12);
+            g.DrawEllipse(new Pen(Color.Red, 2), -10, -10, 20, 20);
+        }
+
+        public override GraphicsPath GetGraphicsPath()
+        {
+            GraphicsPath path = base.GetGraphicsPath();
+            path.AddEllipse(-3, -3, 6, 6);
+            return path;
+        }
+    }
+}
